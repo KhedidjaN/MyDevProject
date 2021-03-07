23926df3a99a485acc20d53fbe0717ed454126c3
khedidjaN <nemrikhadidja@gmail.com>
2021-03-07 09:38:38 -0500

    I am a developper now 4

--- src/java/com/collection/Main.java
+++ src/java/com/collection/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting application");
         while (true){
-            System.out.println("Running");
+            System.out.println("Running1");
             Thread.sleep(2000);
 
         }
